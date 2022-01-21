a4d3be055d8028eb4e5aeded78961799a59f8b41
jaehyunlee <jaehyunlee25@gmail.com>
2022-01-21 19:10:34 +0900

    renew

--- rainbowhills.py
+++ rainbowhills.py
@@ -31,7 +31,7 @@ f.close()
 print(con)
 
 print('2.0. selenium start')
-time.sleep(30)
+time.sleep(40)
 while True:
     print('3.0. while start')
     driver.get('https://www.rainbowhills.co.kr/index.asp')
