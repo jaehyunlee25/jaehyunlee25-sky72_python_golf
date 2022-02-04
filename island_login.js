8de1c736c7302b90c036d9792768810ea6b1780a
jaehyunlee <jaehyunlee25@gmail.com>
2022-02-04 21:33:13 +0900

    renew

--- island_login.js
+++ island_login.js
@@ -1,3 +1,10 @@
-user_id.value = 'newrison';
-user_pw.value = 'ilovegolf778'
+const inputs = document.getElementsByTagName('input');
+const ipts = [];
+Array.from(inputs).forEach((input) => {
+    if(input.type === 'hidden') return;
+    ipts.push(input);
+});
+ipts[0].value = 'newrison';
+ipts[1].value = 'ilovegolf778';
+
 Data_Save();
