bf852dd8d111902f34f9ebf574739a0600427bc5
jaehyunlee <jaehyunlee25@gmail.com>
2022-05-17 10:04:17 +0900

    renew

---
+++ montvert.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime
+import time
+
+time.sleep(0)
+print('\n\n\n\n\n\n== montvert ==')
+
+print('step 1')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+chrome_options.add_argument('window-size=1920x1080')
+chrome_options.add_argument('disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--disable-web-security")
+chrome_options.add_argument("--disable-site-isolation-trials")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+print('step 2')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+f = open('common.js', 'r')
+common = f.read()
+f.close()
+
+f = open('montvert.js', 'r')
+con = f.read()
+f.close()
+
+con = common + con
+
+print('2.0. selenium start')
+while True:
+    print('\n\n== montvert ==')
+    print('3.0. while start')
+    driver.get('https://www.montvertcc.com/reservation/golf')
+    time.sleep(1)
+    driver.execute_script(con)
+
+    print('4.0. while sleep 57')
+    time.sleep(57)
+
+# driver.quit()
